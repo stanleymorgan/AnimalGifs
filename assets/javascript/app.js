7f975c4efd0dcc08c2fc9176868777a588adb8d3
stanleymorgan <stanleymorgan802.gmail.com>
2017-01-16 16:27:40 -0500

    turn in commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,16 +6,14 @@ var animals = ["dog", "cat", "chicken", "cow", "pig", "turkey", "horse", "goat",
 
 		// Deletes the buttons prior to adding new buttons
 		$("#buttonDisplay").empty();
-
-		// Loops through the array of animals
+		
 		for (var i = 0; i < animals.length; i++){
 
-			// Then dynamicaly generates buttons for each animal in the array
-			// Note the jQUery syntax here... 
-		    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+			 
+		    var a = $('<button>') 
 		    a.addClass('animal'); // Added a class 
 		    a.attr('data-name', animals[i]); // Added a data-attribute
-		    a.text(animals[i]); // Provided the initial button text
+		    a.text(animals[i]); 
 		    //a.attr("data-state", "still");
 		    $('#buttonDisplay').append(a); // Added the button to the HTML
 		}
@@ -24,23 +22,22 @@ var animals = ["dog", "cat", "chicken", "cow", "pig", "turkey", "horse", "goat",
 
 		// function to add a new button
 	$('#addAnimal').on('click', function(){
-				// This line of code will grab the input from the textbox
+				//  grab the input from the textbox
 		var newAnimal = $('#animal-input').val().trim();
 
-		// The animal from the textbox is then added to the animals array
+		// The animal from the textbox is added to the animals array
 		animals.push(newAnimal);
 		
 		// run the function to show the buttons
 
 		showButtons();
-
-		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		
 		return false;
 	});
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	    // Adding click event listen listener to all buttons
     $(document).on("click", ".animal" ,function() {
-      // Grabbing and storing the data-name property value from the button
+      
       var animal = $(this).attr("data-name");
 
       // Constructing a queryURL using the animal name
@@ -75,8 +72,7 @@ var animals = ["dog", "cat", "chicken", "cow", "pig", "turkey", "horse", "goat",
             // Appending the paragraph and image tag to the animalDiv
             animalDiv.append(p);
             animalDiv.append(animalImage);
-            // $(this).attr("data-state", "still");
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+           
             $("#gifDisplay").prepend(animalDiv);
           }
         });
@@ -85,9 +81,7 @@ var animals = ["dog", "cat", "chicken", "cow", "pig", "turkey", "horse", "goat",
 	// $("img").on("click", function() {
 	  $(document).on("click", "img" ,function() {
       var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
+      
       if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
